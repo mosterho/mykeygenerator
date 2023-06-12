6817f5efda503069f5f50b24e1d6c6e91907fd37
Marty <mosterho@ramapo.edu>
2023-06-12 16:21:02 -0400

    local commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mykeygenerator
+### Personal OPENSSL commands
+
+The .sh shell works only on Ubuntu; for whatever reason it doesn't work on Git Bash for Windows (seems to hang up when creating a CSR)
---
+++ create-privatepublic-keys.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#set -x      #uncomment this to display more detail from the execution of each line below
+
+echo ""
+#echo "Generating Diffie-Hellman pem file..."
+# Forgot about this to create DH param file if needed (not needed here)
+#openssl dhparam -out ./Results/dh2048.pem 2048
+
+echo ""
+echo "Generating private key..."
+openssl genpkey -algorithm RSA -out ./Results/private.key
+
+echo ""
+echo "Generating public key..."
+openssl pkey -in ./Results/private.key -pubout -out ./Results/publickey.pem
+
+echo ""
+echo "Generating text of public section of private key and public key..."
+openssl pkey -in ./Results/private.key -pubout  -out ./Results/publicdata-of-privatekey.txt
+openssl pkey -in ./Results/publickey.pem -pubin -out ./Results/publickey.txt
+
+echo ""
+echo "Comparing (using DIFF) text of public section of private key and public key..."
+diff -s ./Results/publickey.txt ./Results/publicdata-of-privatekey.txt
+
+## creating a CSR here will also produce an encrypted (password) private key,
+## but on screen only (with these arguments)
+echo ""
+echo "creating new CSR from private key... stand by for prompts!"
+openssl req -new -in ./Results/private.key -out ./Results/privatekey-encrypted-pem.csr -config ./CNF/ESIS-CA-request.cnf
+##openssl req -new -in ./Results/private.key -out ./Results/privatekey-encrypted.pem
+
+echo ""
+echo "Verifying CSR..."
+openssl req -in ./Results/privatekey-encrypted-pem.csr -text -verify -noout
+
+echo "Sleeping for 600 seconds, CTRL-c when ready to close this window."
+sleep 600
